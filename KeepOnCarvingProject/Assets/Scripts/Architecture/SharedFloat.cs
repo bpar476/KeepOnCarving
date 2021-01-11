3048d31dee9a1344ba0c2faccc6fbc2437e99223
Ben Partridge <ben.partridge249@gmail.com>
2021-01-12 08:59:29 +1100

    Add operators for primitive floats to SharedFloat ScriptableObject

--- KeepOnCarvingProject/Assets/Scripts/Architecture/SharedFloat.cs
+++ KeepOnCarvingProject/Assets/Scripts/Architecture/SharedFloat.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "SharedFloat", menuName = "KeepOnCarving/SharedFloat", order = 50)]
-public class SharedFloat : ScriptableObject {
+public class SharedFloat : ScriptableObject
+{
 
 #if UNITY_EDITOR
     [Multiline]
@@ -13,7 +14,58 @@ public class SharedFloat : ScriptableObject {
 
     public float Value;
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         Value = defaultValue;
+    }
+
+    public static float operator *(SharedFloat a, float b)
+    {
+        return a.Value * b;
+    }
+
+    public static float operator +(SharedFloat a, float b)
+    {
+        return a.Value + b;
+    }
+
+    public static float operator -(SharedFloat a, float b)
+    {
+        return a.Value - b;
+    }
+
+    public static float operator -(float a, SharedFloat b)
+    {
+        return a - b.Value;
+    }
+
+    public static bool operator >(SharedFloat a, float b)
+    {
+        return a.Value > b;
+    }
+
+    public static bool operator <(SharedFloat a, float b)
+    {
+        return a.Value < b;
+    }
+
+    public static bool operator ==(SharedFloat a, float b)
+    {
+        return a.Value == b;
+    }
+
+    public static bool operator !=(SharedFloat a, float b)
+    {
+        return a.Value != b;
+    }
+
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+
+    public override bool Equals(object other)
+    {
+        return Value.Equals(other);
     }
 }
