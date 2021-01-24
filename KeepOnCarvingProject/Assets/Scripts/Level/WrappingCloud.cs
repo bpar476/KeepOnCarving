c2f6e29239b05eb43a11617dccd64de1224e6aa6
Ben Partridge <ben.partridge249@gmail.com>
2021-01-25 09:33:19 +1100

    Implement scrolling animated clouds shader

---
+++ KeepOnCarvingProject/Assets/Scripts/Level/WrappingCloud.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class WrappingCloud : MonoBehaviour
+{
+
+    private static readonly string MAT_PROP_DIRECTION = "_Direction";
+    private static readonly string MAT_PROP_WRAP_LENGTH = "_WrapLength";
+    private static readonly string MAT_PROP_OFFSET = "_Offset";
+
+    [SerializeField]
+    private Vector3 direction;
+    [SerializeField]
+    private Vector3 wrapLength;
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        var mat = GetComponent<SpriteRenderer>().material;
+        mat.SetVector(MAT_PROP_DIRECTION, direction);
+        mat.SetVector(MAT_PROP_WRAP_LENGTH, wrapLength);
+        mat.SetVector(MAT_PROP_OFFSET, offset);
+    }
+
+}
