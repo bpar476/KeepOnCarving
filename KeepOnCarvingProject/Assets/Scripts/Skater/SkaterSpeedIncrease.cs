fef738c0bfb81d3a443a3acea972451e61f912e9
Ben Partridge <ben.partridge249@gmail.com>
2021-01-26 17:47:55 +1100

    Implement skater speeding up as score increases

---
+++ KeepOnCarvingProject/Assets/Scripts/Skater/SkaterSpeedIncrease.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class SkaterSpeedIncrease : MonoBehaviour
+{
+    [SerializeField]
+    private SharedFloat skaterScore;
+
+    [SerializeField]
+    private SharedFloat skaterSpeed;
+
+    /// <summary>
+    /// The amount of score the skater must achieve between increases in skater speed
+    /// </summary>
+    [SerializeField]
+    private int speedIncreaseScore;
+
+    /// <summary>
+    /// The amount that the skater increases in speed after travelling the Speed Increase Distance
+    /// </summary>
+    [SerializeField]
+    private float speedIncreaseAmount;
+
+    private float nextIncreaseScore;
+
+    private void Awake()
+    {
+        nextIncreaseScore = speedIncreaseScore;
+    }
+
+    void Update()
+    {
+        if (skaterScore > nextIncreaseScore)
+        {
+            Debug.Log("Increasing skater speed");
+            nextIncreaseScore += speedIncreaseScore;
+            skaterSpeed.Value += speedIncreaseAmount;
+        }
+    }
+}
