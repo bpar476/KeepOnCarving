6c754a898bd947b12c12c4ae1d5c1aa04c599617
Ben Partridge <ben.partridge249@gmail.com>
2021-01-27 08:29:28 +1100

    Implement high score tracking and naive high score UI

---
+++ KeepOnCarvingProject/Assets/Scripts/Systems/HighScoreTracker.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "HighScoreTracker", menuName = "KeepOnCarving/High Score Tracker")]
+public class HighScoreTracker : ScriptableObject
+{
+    private static readonly string PLAYER_PREF_KEY_HIGH_SCORE = "highScore";
+
+    public int HighScore { get; private set; }
+
+    [SerializeField]
+    private EventBusContainer busContainer;
+
+    [SerializeField]
+    private SharedFloat skaterScore;
+
+    private System.Guid eventListenerId;
+
+    private void OnEnable()
+    {
+        HighScore = PlayerPrefs.GetInt(PLAYER_PREF_KEY_HIGH_SCORE, 0);
+        var eventListenerId = busContainer.Bus.ListenTo<SkaterCrashEvent>(SetNewHighScore);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetInt(PLAYER_PREF_KEY_HIGH_SCORE, HighScore);
+        busContainer.Bus.UnsubscribeFrom<SkaterCrashEvent>(eventListenerId);
+    }
+
+    private void SetNewHighScore(SkaterCrashEvent _)
+    {
+        var score = (int)skaterScore.Value;
+        if (score > HighScore)
+        {
+            HighScore = score;
+        }
+    }
+}
---
+++ KeepOnCarvingProject/Assets/Scripts/UI/HighScoreUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HighScoreUI : MonoBehaviour
+{
+    [SerializeField]
+    private HighScoreTracker highScoreTracker;
+
+    private TMPro.TMP_Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<TMPro.TMP_Text>();
+    }
+
+    private void Update()
+    {
+        text.text = highScoreTracker.HighScore.ToString();
+    }
+}
